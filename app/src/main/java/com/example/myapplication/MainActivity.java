513b7c3ed2c6ce31a4866ada8548e66c24de1511
Your Name <you@example.com>
2019-06-04 22:56:22 +0800

    增加弹出

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         
         Toast.makeText(this,"test", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,"test", Toast.LENGTH_SHORT).show();
     }
 }
